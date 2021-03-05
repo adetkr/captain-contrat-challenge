7cc7a544cfa15f2a6b9b3e5780346741c7c82f85
ademola toukourou <tkradm@yahoo.fr>
2021-03-05 13:46:22 +0100

    add validation to character model

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,28 +4,38 @@ class CharactersController < ApplicationController
   end
 
   def create
+    @character = Character.new(params_character)
 
+    if @character.save
+      redirect_to characters_path
+    else
+      render :new
+    end
   end
 
   def index
-
+    @characters = Character.all
   end
 
   def show
-
   end
 
   def edit
-
   end
 
   def update
-
   end
 
   def destroy
-
   end
 
+  private
 
+  def set_character
+    @character = Character.find(params[:id])
+  end
+
+  def params_character
+    params.require(:character).permit(:name, :life, :attack)
+  end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,5 @@
 class Character < ApplicationRecord
+  validates :name , uniqueness: true, presence:true
+  validates :life , presence: true
+  validates :attack , presence: true
 end
