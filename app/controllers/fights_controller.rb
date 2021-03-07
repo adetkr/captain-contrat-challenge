f39a28fbb4963b042882cb6c5c10fd35ea550653
ademola toukourou <tkradm@yahoo.fr>
2021-03-07 12:50:59 +0100

    added a fight simulator service and remove fight logic from controller

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -1,7 +1,6 @@
 class FightsController < ApplicationController
   def index
     @fights = Fight.all
-
   end
 
   def show
@@ -9,7 +8,7 @@ class FightsController < ApplicationController
   end
 
   def new
-    @fight = Fight.new()
+    @fight = Fight.new
     @fight.players.build
     @fight.players.build
   end
@@ -17,30 +16,7 @@ class FightsController < ApplicationController
   def create
     @fight = Fight.new(fight_params)
     if @fight.save
-      # implement fight simulation
-      player1 = @fight.players[0]
-      player2 = @fight.players[1]
-
-      while player1.character.life > 0
-        #player 1 attacks
-        attack(player1, player2)
-        #check survivor
-        break if player2.character.life <= 0
-        # player 2 attacks
-        attack(player2, player1)
-
-      end
-      player1.win = player2.character.life <= 0
-      player2.win = !player1.win
-
-      if player1.win
-        add_log(@fight, " #{player1.character.name } win ")
-      else
-        add_log(@fight, " #{player2.character.name } win ")
-      end
-      player1.save
-      player2.save
-
+      FightSimulator.new(@fight).simulate_fight
       redirect_to fight_path(@fight)
     else
       render :new
@@ -48,31 +24,6 @@ class FightsController < ApplicationController
   end
 
   private
-
-  def evade?(player)
-    rand < player.character.speed/100.0
-  end
-
-  def add_log(fight, move_description)
-    log = FightLog.new()
-    log.fight = fight
-    log.move_description = move_description
-    log.save
-  end
-
-  def attack(player1, player2)
-    if evade?(player2)
-      add_log(@fight, " #{player2.character.name } evaded from  #{player1.character.name} attack ")
-    else
-      if player2.shield.power > 0
-        player2.shield.power -= (player1.character.attack + player1.weapon.power) / 10
-        add_log(@fight,  " #{player2.character.name } blocked attack from #{player1.character.name } with #{player2.shield.name} shield ")
-      else
-        player2.character.life -= (player1.character.attack + player1.weapon.power) / 10
-        add_log(@fight, " #{player1.character.name } impact #{(player1.character.attack + player1.weapon.power) / 10} damage to #{player2.character.name }")
-      end
-    end
-  end
 
   def fight_params
     params.require(:fight).permit(players_attributes: [:id, :character_id, :weapon_id, :shield_id])
---
+++ app/services/fight_simulator.rb
@@ -0,0 +1,58 @@
+class FightSimulator
+  def initialize(fight)
+    @fight = fight
+  end
+
+  def simulate_fight
+    # implement fight simulation
+    player1 = @fight.players[0]
+    player2 = @fight.players[1]
+
+    while player1.character.life > 0
+      #player 1 attacks
+      attack(player1, player2)
+      #check survivor
+      break if player2.character.life <= 0
+      # player 2 attacks
+      attack(player2, player1)
+
+    end
+    player1.win = player2.character.life <= 0
+    player2.win = !player1.win
+
+    if player1.win
+      add_log(@fight, " #{player1.character.name } win ")
+    else
+      add_log(@fight, " #{player2.character.name } win ")
+    end
+    player1.save
+    player2.save
+  end
+
+  private
+
+  def evade?(player)
+    rand < player.character.speed/100.0
+  end
+
+  def add_log(fight, move_description)
+    log = FightLog.new()
+    log.fight = fight
+    log.move_description = move_description
+    log.save
+  end
+
+  def attack(player1, player2)
+    if evade?(player2)
+      add_log(@fight, " #{player2.character.name } evaded from  #{player1.character.name} attack ")
+    else
+      if player2.shield.power > 0
+        player2.shield.power -= (player1.character.attack + player1.weapon.power) / 10
+        add_log(@fight,  " #{player2.character.name } blocked attack from #{player1.character.name } with #{player2.shield.name} shield ")
+      else
+        player2.character.life -= (player1.character.attack + player1.weapon.power) / 10
+        add_log(@fight, " #{player1.character.name } impact #{(player1.character.attack + player1.weapon.power) / 10} damage to #{player2.character.name }")
+      end
+    end
+  end
+end
