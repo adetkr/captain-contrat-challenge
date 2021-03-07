4852ccd04acf477aae2d84dda7550e8206272119
ademola toukourou <tkradm@yahoo.fr>
2021-03-07 13:28:03 +0100

    add validation to character model

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,10 +1,20 @@
 class Character < ApplicationRecord
   validates :name , uniqueness: true, presence:true
   validates :life , presence: true
-  validates :attack , presence: true
-  validates :speed , presence: true
+  validates :attack , presence: true, inclusion: { in: 1..100 }
+  validates :speed , presence: true, inclusion: { in: 1..100 }
+
+  validate :total_points
 
   has_many :players
   has_many :fights, through: :players
   has_one_attached :photo
+
+  def total_points
+    if !life.nil? && !attack.nil? && !speed.nil?
+      if life + attack + speed > 200
+        errors.add(:base, "Total charasteristic points can't excee 200")
+      end
+    end
+  end
 end
